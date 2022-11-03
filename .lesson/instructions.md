d88085478670a6379730fc244be1882c1bf3492c
David M. Wilson <davidmichaelwilson81@gmail.com>
2022-11-03 14:23:43 -0500

    added from replit

---
+++ .lesson/instructions.md
@@ -0,0 +1,4 @@
+# Instructions  
+
+ When the user clicks the "Buy It" button, show "Something went terribly wrong. Please try again." in the <p> that has the id="error".
+  
---
+++ script.js
@@ -0,0 +1,6 @@
+let error = document.getElementById("error")
+let message = "Something went terribly wrong. Please try again."
+
+function runMessage() {
+  error.innerText = message
+}
